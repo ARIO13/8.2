8a97a8865039c2e7734bb5e101a7401f6fe13fd3
ARIO13 <110298616+ARIO13@users.noreply.github.com>
2022-08-01 13:02:25 +0300

    Add files via upload

---
+++ UnitsEnumeration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MeasuringDevice
+{
+    //  TODO: Implement the Units enumeration.
+    public enum Units
+    {
+        Metric, Imperial
+    }
+}
